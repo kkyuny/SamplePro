cd7ad7eaa380fe37dd2fe08186a29b29e6b453d5
kyun9911 <kyun9911@gmail.com>
2022-03-07 12:14:39 +0900

    hotfix Branch 문자열 출력

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -3,7 +3,9 @@ public class GitTestMain {
 
 	public static void main(String[] args) {
 		System.out.println("GitTestMain");
-
+		System.out.println("Master Branch");
+		
+		System.out.println("hotfix Branch");
 	}
 
 }
